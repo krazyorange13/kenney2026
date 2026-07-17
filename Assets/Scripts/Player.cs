53a44ef323813dd4a33bfc9106e00765f01b3cad
mattseq <matthewsequeir@gmail.com>
2026-07-17 16:39:04 -0500

    add tuff music

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,32 +7,36 @@ public class Player : MonoBehaviour
   public LayerMask groundLayer;
   public float movementSpeed = 5.0f;
 
-    void Update()
-    {
-      Vector2 mousePos = Mouse.current.position.ReadValue();
-      Ray ray = camera.ScreenPointToRay(mousePos);
-      RaycastHit hit;
+  void Update()
+  {
+    Movement();
+  }
 
-      if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer)) {
-        Vector3 target = hit.point;
-        target.y = transform.position.y;
-      
-        if (target.magnitude > 0.01f) {
+  void Movement() {
+    Vector2 mousePos = Mouse.current.position.ReadValue();
+    Ray ray = camera.ScreenPointToRay(mousePos);
+    RaycastHit hit;
 
-          transform.position = Vector3.MoveTowards(
-              transform.position,
-              target,
-              movementSpeed * Time.deltaTime
-          );
+    if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer)) {
+      Vector3 target = hit.point;
+      target.y = transform.position.y;
+    
+      if (target.magnitude > 0.01f) {
 
-          Vector3 direction = target - transform.position;
-          Quaternion targetRotation = Quaternion.LookRotation(direction);
-          transform.rotation = Quaternion.Slerp(
-              transform.rotation,
-              targetRotation,
-              10f * Time.deltaTime
-          );
-        }
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            target,
+            movementSpeed * Time.deltaTime
+        );
+
+        Vector3 direction = target - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            targetRotation,
+            10f * Time.deltaTime
+        );
       }
     }
+  }
 }
