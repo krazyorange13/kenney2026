385cb0ae25489fb0f50fd2d232635b744ff74541
mattseq <matthewsequeir@gmail.com>
2026-07-17 15:05:21 -0500

    player movement with isometric camera

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+  public Camera camera;
+  public LayerMask groundLayer;
+  public float movementSpeed = 5.0f;
+
+    void Update()
+    {
+      Vector2 mousePos = Mouse.current.position.ReadValue();
+      Ray ray = camera.ScreenPointToRay(mousePos);
+      RaycastHit hit;
+
+      if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer)) {
+        Vector3 target = hit.point;
+        target.y = transform.position.y;
+      
+        if (target.magnitude > 0.01f) {
+
+          transform.position = Vector3.MoveTowards(
+              transform.position,
+              target,
+              movementSpeed * Time.deltaTime
+          );
+
+          Vector3 direction = target - transform.position;
+          transform.rotation = Quaternion.LookRotation(direction);
+        }
+      }
+    }
+}
