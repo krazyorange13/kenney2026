f098841fea94465f3a81e261f125610fdee7111d
mattseq <matthewsequeir@gmail.com>
2026-07-17 17:15:11 -0500

    smooth camera movement, 60 deg camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,9 +4,17 @@ public class CameraMovement : MonoBehaviour
 {
   public GameObject player;
   public Vector3 offset;
+  public float smoothSpeed = 0.05f;
+
+  private Vector3 velocity = Vector3.zero;
 
   void LateUpdate()
   {
-    transform.position = player.transform.position + offset;
+    transform.position = Vector3.SmoothDamp(
+        transform.position,
+        player.transform.position + offset,
+        ref velocity,
+        smoothSpeed
+    );
   }
 }
