9d84ddafea5ad46f490e59ead505b0ca1f548f17
krazyorange13 <77130315+krazyorange13@users.noreply.github.com>
2026-07-17 19:05:18 -0500

    add city sparsity (rough)

--- Assets/Scripts/GenerateCity.cs
+++ Assets/Scripts/GenerateCity.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,8 +8,11 @@ public class GenerateCity : MonoBehaviour
     public GameObject buildingFillPrefab;
     public GameObject roadIntersectionPrefab;
     public GameObject roadStraightPrefab;
+    public GameObject roadStraightSinglePrefab;
 
     public float spread = 1.0f;
+    public float sparsity = 0.1f;
+    public float randomness = 0.25f;
 
     public int cityWidth;
     public int cityHeight;
@@ -16,7 +20,17 @@ public class GenerateCity : MonoBehaviour
     public float tileWidth = 25.0f;
     public float tileHeight = 25.0f;
 
+    private GameObject[,] tiles;
+
     void Start()
+    {
+        tiles = new GameObject[cityWidth, cityHeight];
+
+        Populate();
+        Decimate();
+    }
+
+    void Populate()
     {
         for (int i = 0; i < cityWidth; i++)
         {
@@ -65,15 +79,54 @@ public class GenerateCity : MonoBehaviour
 
                 GameObject newTile = Instantiate(prefab, position, rotation, transform);
                 newTile.transform.localScale = scale;
+
+                tiles[i, j] = newTile;
             }
         }
+    }
+
+    void Decimate()
+    {
+        for (int i = 0; i < cityWidth; i++)
+        {
+            for (int j = 0; j < cityHeight; j++)
+            {
+                if (!(i % 2 == 0 && j % 2 == 0))
+                {
+                    continue;
+                }
+
+                if (UnityEngine.Random.value <= sparsity)
+                {
+                    Destroy(tiles[i, j]);
+                    // ReplaceRoad(i - 1, j);
+                    // ReplaceRoad(i + 1, j);
+                    // ReplaceRoad(i, j - 1);
+                    // ReplaceRoad(i, j + 1);
+                }
+            }
+        }
+    }
+
+    void ReplaceRoad(int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= cityWidth || j >= cityHeight)
+        {
+            return;
+        }
+
+        Vector3 position = tiles[i, j].transform.position;
+        Quaternion rotation = Quaternion.identity;
+        Vector3 scale = tiles[i, j].transform.localScale;
+        tiles[i, j] = Instantiate(roadStraightSinglePrefab, position, rotation, transform);
+        tiles[i, j].transform.localScale = scale;
     }
 
     GameObject GetBuilding(int i, int j)
     {
         int height;
         height = Mathf.FloorToInt(GetBuildingHeight(i, j, 0.0f, (float)buildingPrefabs.Count));
-        if (UnityEngine.Random.value >= 0.25)
+        if (UnityEngine.Random.value >= randomness)
         {
             height += UnityEngine.Random.Range(-1, 2);
         }
