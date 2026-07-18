80c8b13eebc5a2eb1082afafcf9a84cb5802daff
mattseq <matthewsequeir@gmail.com>
2026-07-18 15:59:59 -0500

    camera zoom out by player scale

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
   public GameObject player;
-  public Vector3 offset = new Vector3(-6f, 14f, -3f);
+  private Vector3 offset = new Vector3(-75f, 150f, -35f);
+  public float cameraDistance = 10f;
+  public float sizeMultiplier = 2f;
   public float smoothSpeed = 0.02f;
 
   private Vector3 velocity = Vector3.zero;
 
   void LateUpdate()
   {
+    float playerSize = player.GetComponent<Edible>().scale;
+
+    this.GetComponent<Camera>().orthographicSize = cameraDistance + playerSize * sizeMultiplier;
+
+    Vector3 targetPosition = player.transform.position + offset;
+
     transform.position = Vector3.SmoothDamp(
         transform.position,
-        player.transform.position + offset,
+        targetPosition,
         ref velocity,
         smoothSpeed
     );
